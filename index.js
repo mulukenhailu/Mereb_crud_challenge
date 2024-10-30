27d9ec56cc4fabbcd372d1934ee0243e72b79dda
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2024-10-30 21:55:04 +0300

    Refactor naming conventions for consistency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const personRoutes = require('./routes/route');
+const personRoutes = require('./routes/personRoute');
 
 const app = express();
 const PORT = 3000;
